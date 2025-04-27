d275225ec60adc370396e8a6262919efbdab734d
neandrson <a2361@yandex.ru>
2025-04-27 12:41:33 +0300

    Create timeout.go

---
+++ pkg/timeout/timeout.go
@@ -0,0 +1,21 @@
+package timeout
+
+import (
+	"context"
+	"time"
+)
+
+type Timeout struct {
+	Timer  *time.Timer
+	Ctx    context.Context
+	Cancel context.CancelFunc
+}
+
+func NewTimeout(duration time.Duration) *Timeout {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &Timeout{
+		Timer:  time.NewTimer(duration),
+		Ctx:    ctx,
+		Cancel: cancel,
+	}
+}
