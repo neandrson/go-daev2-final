d36bbba955e179282a831e3136f3315596a0c0e1
neandrson <a2361@yandex.ru>
2025-04-29 21:43:42 +0300

    Create config.go

---
+++ orchestrator/internal/config/config.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string           `yaml:"env"`
+	DatabaseUrl string           `yaml:"database_url"`
+	Secret      string           `yaml:"secret"`
+	GRPC        GRPCConfig       `yaml:"grpc"`
+	GRPCClient  GRPCClientConfig `yaml:"grpc_client"`
+	HTTP        HTTPConfig       `yaml:"http"`
+	TokenTTL    time.Duration    `yaml:"token_ttl"`
+}
+
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+type HTTPConfig struct {
+	Port int `yaml:"port"`
+}
+
+type GRPCClientConfig struct {
+	Addr         string `yaml:"sso_addr"`
+	RetriesCount int    `yaml:"retries_count"`
+}
+
+func MustLoad() *Config {
+	configPath := fetchConfigPath()
+	if configPath == "" {
+		panic("config path is empty")
+	}
+
+	return MustLoadPath(configPath)
+}
+
+func MustLoadPath(configPath string) *Config {
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches config path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
