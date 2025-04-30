b3a95ae0e07a8aa1fc04b8e200d8223e85dcbae7
neandrson <a2361@yandex.ru>
2025-04-30 20:47:37 +0300

    Update server.go

--- orchestrator/internal/http-server/server/server.go
+++ orchestrator/internal/http-server/server/server.go
@@ -68,8 +68,8 @@ func New(log *slog.Logger, storage httpservice.ExpressionStorage, port int, secr
 	serveMux.Handle("/expression", middleware.ValidateToken(middleware.ValidateExpressionMiddleware(http.HandlerFunc(server.EvaluateExpression)), server.secret)).Methods("POST")
 	serveMux.Handle("/expression", middleware.ValidateToken(http.HandlerFunc(server.GetExpressionById), server.secret)).Methods("GET")
 	serveMux.Handle("/all_expressions", middleware.ValidateToken(http.HandlerFunc(server.GetExpressionsForUser), server.secret)).Methods("GET")
-	serveMux.HandleFunc("/login", server.Login).Methods("POST")
-	serveMux.HandleFunc("/register", server.Register).Methods("POST")
+	serveMux.HandleFunc("/api/v1/login", server.Login).Methods("POST")
+	serveMux.HandleFunc("/api/v1/register", server.Register).Methods("POST")
 	serveMux.HandleFunc("/agents_state", server.GetAgentStates).Methods("GET")
 
 	// Хэндлеры для API
