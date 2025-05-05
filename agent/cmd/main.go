5d396c99acc11d5c2d2f729d190a5f4c70883d3d
neandrson <62153258+neandrson@users.noreply.github.com>
2025-05-05 10:07:15 +0300

    Create main.go

---
+++ agent/cmd/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/neandrson/go-daev2-final/agent/internal/app"
+	"github.com/neandrson/go-daev2-final/agent/internal/config"
+	"github.com/neandrson/go-daev2-final/agent/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	application := app.New(log, cfg.GRPCClient.Addr, cfg.GRPCClient.RetriesCount, cfg.CountCalcs, map[string]time.Duration{
+		"+": cfg.Durations.Plus,
+		"-": cfg.Durations.Minus,
+		"*": cfg.Durations.Mult,
+		"/": cfg.Durations.Del,
+		"^": cfg.Durations.Pow,
+	})
+
+	application.GRPCClient.MustRun()
+
+	// Graceful stop
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+
+	<-stop
+
+	application.GRPCClient.Stop()
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettyLogger()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettyLogger() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
