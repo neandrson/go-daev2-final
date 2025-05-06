be534b4accb01fe5b46b75c3802587307ff3dc34
neandrson <a2361@yandex.ru>
2025-05-06 21:30:29 +0300

    Update scan_test.go

--- shunting-yard/scan_test.go
+++ shunting-yard/scan_test.go
@@ -18,11 +18,11 @@ func TestScan(t *testing.T) {
 		t.Fatalf("Expected %v, Got %v", expected, values)
 	}
 
-	values, err = Scan("123^456")
+	values, err = Scan("123+456")
 	if err != nil {
 		panic(err)
 	}
-	expected = []string{"123", "^", "456"}
+	expected = []string{"123", "+", "456"}
 	if !reflect.DeepEqual(values, expected) {
 		t.Fatalf("Expected %v, Got %v", expected, values)
 	}
